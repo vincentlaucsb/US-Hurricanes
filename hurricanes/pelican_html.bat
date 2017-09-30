90142e3d4723722d01c406d055aa8e197babf810
vincentlaucsb <vincela9@gmail.com>
2017-09-30 16:09:01 -0700

    Added history of hurricane tracking

--- hurricanes/pelican_html.bat
+++ hurricanes/pelican_html.bat
@@ -3,3 +3,5 @@ Rscript -e "rmarkdown::render('hurricane_severity.Rmd', output_format='html_docu
 Rscript -e "rmarkdown::render('hurricane_us.Rmd', output_format='html_document')"
 
 python pelican_convert.py
+
+pause
--- views/functions.sql
+++ views/functions.sql
@@ -7,3 +7,18 @@ CREATE OR REPLACE FUNCTION array_max(input anyarray)
     RETURNS anyelement AS
 $$ SELECT max(x) FROM unnest(input) x; $$
 LANGUAGE sql;
+
+-- Return the path of a hurricane along with associated data
+/*
+Usage example
+    SELECT * FROM hurricane_path('CAROL', 1954);
+*/
+CREATE OR REPLACE FUNCTION hurricane_path(name_ text, season_ int) RETURNS
+    TABLE(lon float, lat float, wind_max_kt float, datetime text, sshs text) AS
+$$
+WITH hurricane AS (SELECT path_intensity FROM hurricane_summary
+WHERE name LIKE name_ AND season = season_)
+SELECT * FROM json_to_recordset(array_to_json((SELECT * FROM hurricane)))
+AS x(lon float, lat float, wind_max_kt float, time text, sshs text)
+$$
+LANGUAGE sql;
